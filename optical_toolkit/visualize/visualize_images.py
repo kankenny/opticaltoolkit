f1e7521deeb62c5d46eb804c7c613cfa098e82ff
kenny <97374837+kankenny@users.noreply.github.com>
2024-10-10 21:06:56 +0000

    refactor: add option to pass image size

--- optical_toolkit/visualize/visualize_images.py
+++ optical_toolkit/visualize/visualize_images.py
@@ -2,18 +2,16 @@ import numpy as np
 
 from .core import min_max_normalize
 
-SIZE = 200
 
-
-def visualize_images(X, channels=3):
+def visualize_images(X, image_size=200, channels=3):
     """Create a sprite image from input data."""
-    matrix_images = _vector_to_matrix(X, channels)
+    matrix_images = _vector_to_matrix(X, image_size, channels)
     return _create_sprite_image(matrix_images, channels)
 
 
-def _vector_to_matrix(images, channels):
+def _vector_to_matrix(images, image_size, channels):
     """Convert a vector of images to a 4D matrix."""
-    return np.reshape(images, (-1, SIZE, SIZE, channels))
+    return np.reshape(images, (-1, image_size, image_size, channels))
 
 
 def _create_sprite_image(images, channels):
@@ -28,8 +26,8 @@ def _create_sprite_image(images, channels):
             if this_filter < images.shape[0]:
                 this_img = min_max_normalize(images[this_filter])
                 sprite_image[
-                    i * img_h: (i + 1) * img_h,
-                    j * img_w: (j + 1) * img_w,
+                    i * img_h : (i + 1) * img_h,
+                    j * img_w : (j + 1) * img_w,
                 ] = this_img
 
     return sprite_image
