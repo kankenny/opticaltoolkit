f1cafd4cc78ea8939a9f367d103372e3a7beadf2
kenny <97374837+kankenny@users.noreply.github.com>
2024-10-11 19:20:25 +0000

    chore: add unit test for 2d embedding

--- tests/test_visualizations.py
+++ tests/test_visualizations.py
@@ -2,7 +2,10 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pytest
+from sklearn.datasets import load_digits
 
+from optical_toolkit.visualize.visualize_embeddings import visualize_embeddings
 from optical_toolkit.visualize.visualize_images import visualize_images
 
 
@@ -20,3 +23,13 @@ def test_visualize_images():
     bordered_grid = visualize_images(noise_images, height * 2, border_size=10)
     output_path = os.path.join("examples", "noise_image_grid_bordered.png")
     plt.imsave(output_path, bordered_grid)
+
+
+def test_tsne_embedding():
+    digits = load_digits()
+    X = digits.images
+
+    try:
+        visualize_embeddings(X)
+    except Exception as e:
+        pytest.fail(f"visualize_embeddings raised an exception: {e}")
