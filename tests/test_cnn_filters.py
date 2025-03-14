6e7895061ff401fef7298182e691e7c4fd4e7d5b
kenny <97374837+kankenny@users.noreply.github.com>
2025-03-14 21:25:24 +0000

    tests: reduce redundancy and change example layers to visualize

--- tests/test_cnn_filters.py
+++ tests/test_cnn_filters.py
@@ -5,11 +5,7 @@ def test_filters_from_pretrained_model():
     model_name = "xception"
     dir_name = "examples/insights"
 
-    display_filters(model_name, layer_name="block1_conv1",
-                    output_path=f"{dir_name}/block1_conv1_layer_filters.png")
-    display_filters(model_name, layer_name="block5_sepconv1",
-                    output_path=f"{dir_name}/block5_sepconv1_layer_filters.png")
-    display_filters(model_name, layer_name="block9_sepconv1",
-                    output_path=f"{dir_name}/block9_sepconv1_layer_filters.png")
-    display_filters(model_name, layer_name="block14_sepconv1",
-                    output_path=f"{dir_name}/block14_sepconv1_layer_filters.png")
+    layer_names = ["block2_sepconv1", "block5_sepconv1", "block9_sepconv1", "block14_sepconv1"]
+
+    for layer_name in layer_names:
+        display_filters(model_name, layer_name=layer_name, output_path=f"{dir_name}/{layer_name}_layer_filters.png")
